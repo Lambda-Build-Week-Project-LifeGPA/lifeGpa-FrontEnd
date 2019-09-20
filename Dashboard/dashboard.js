f6f7c11db310f6f52722db015fc9edfde27c38f2
Mysti Dyse <mystidyse@gmail.com>
2019-09-20 13:40:22 -0700

    added import of dependencies. react and use state, added route for when we get to the point of routing each page back to the dashboard, and added the import and paths for the components. just creating the framework for now.

---
+++ Dashboard/dashboard.js
@@ -0,0 +1,8 @@
+// imports -- some used some not yet used
+import React, { useState } from 'react'
+import { Route } from 'react-router-dom' /*to be used for when we are routing the different pages to the dashboard */
+// imports of components
+import NavBarComponent from './Components/NavBarComponent'
+import OverallGPA from './Components/OverallGPA'
+import habitCard from './Components/habitCard'
+import buttonComponent from './Components/buttonComponent'
